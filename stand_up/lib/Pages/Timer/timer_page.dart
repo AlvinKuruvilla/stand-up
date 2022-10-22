61e8c52907b0ead432bb692d240a354a9a1f6eef
Alvin Kuruvilla <alvineasokuruvilla@gmail.com>
2022-10-21 23:58:09 -0400

    move settings to sidebar

--- stand_up/lib/Pages/Timer/timer_page.dart
+++ stand_up/lib/Pages/Timer/timer_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:stand_up/Widgets/animated_clock.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:stand_up/Widgets/in_progress.dart';
 
 class TimerPage extends StatefulWidget {
@@ -42,39 +41,21 @@ class _TimerPageState extends State<TimerPage> {
                       MaterialPageRoute(
                           builder: (context) => const InProgressPage()));
                 },
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text("Timer Settings"),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const InProgressPage()));
+                },
               )
             ],
           ),
         ),
         backgroundColor: Colors.white,
-        body: const CountDownTimer(),
-        floatingActionButton: Container(
-          padding: const EdgeInsets.only(top: 40),
-          child: Align(
-            alignment: Alignment.topRight,
-            child: FloatingActionButton(
-              shape: const RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(0),
-                      topRight: Radius.circular(0),
-                      bottomLeft: Radius.circular(0),
-                      bottomRight: Radius.circular(45))),
-              elevation: 0.0,
-              backgroundColor: Colors.white,
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const InProgressPage()));
-              },
-              child: const Icon(
-                CupertinoIcons.settings_solid,
-                color: Colors.lightBlue,
-                size: 32,
-              ),
-            ),
-          ),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+        body: const CountDownTimer());
   }
 }
